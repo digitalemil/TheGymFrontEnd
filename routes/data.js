09d60a247cce1797444d752b180974a4de33f14a
Emil Andreas Siemes <emil@Emils-MacBook-Air.local>
2019-12-25 18:08:41 +0100

    auth in /data

--- routes/data.js
+++ routes/data.js
@@ -3,12 +3,21 @@ let router = express.Router();
 global.hr= "---";
 global.lon= "---";
 global.lat= "---";
+var passwd= require('./passwd.json');
+obj[passwd.user]= passwd.password;console.log(obj);
+
 
 /* GET Data */
 router.get('/', function (req, res, next) {
     global.hr= req.query.hr;
     global.lon= req.query.lon;
     global.lat= req.query.lat;
+    if(passwd.user!= req.query.user || passwd.password!= req.query.password) {
+        res.write("Unauthorized");
+        res.end();
+        return;
+    }
+
     res.write("Thank you.\n");
   res.end();
   //res.render('home', { title: 'The Gym', hr:hr, lon:lon, lat:lat });
