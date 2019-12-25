8723debd3a2570b4786a1b1aa69f4d14d3fc6793
Emil Andreas Siemes <emil@Emils-MacBook-Air.local>
2019-12-25 16:49:32 +0100

    enforce https

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var config= require('./config.json');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
 function extractProfile(profile) {
   console.log("User authenticated: "+profile.id+" "+profile.displayName);
@@ -61,7 +68,7 @@ const sessionConfig = {
   signed: true
 };
 
-
+app.use(requireHTTPS);
 app.use(session(sessionConfig));
 
 // view engine setup
