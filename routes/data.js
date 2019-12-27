ce5fdd399a835bb1831b989579f0e54dd102d8c6
Emil A. Siemes <digitalemil@googlemail.com>
2019-12-27 13:18:49 +0100

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -3,6 +3,8 @@ let router = express.Router();
 global.hr= "---";
 global.lon= "---";
 global.lat= "---";
+global.user="---";
+
 var passwd= require('../passwd.json');
 
 /* GET Data */
@@ -15,10 +17,23 @@ router.get('/', function (req, res, next) {
     global.hr= req.query.hr;
     global.lon= req.query.lon;
     global.lat= req.query.lat;
-  
+    global.user= req.query.user;  
   
     res.write("Thank you.\n");
   res.end();
+  //res.render('home', { title: 'The Gym', hr:hr, lon:lon, lat:lat });
+});
+
+/* Fetch Data */
+router.get('/fetch', function (req, res, next) {
+    if(passwd.user!= req.query.user || passwd.password!= req.query.password) {
+        res.write("Unauthorized");
+        res.end();
+        return;
+    }
+
+    res.write(global.user+","+global.hr+","+global.lon+","global.lat+"\n");
+    res.end();
   //res.render('home', { title: 'The Gym', hr:hr, lon:lon, lat:lat });
 });
 
